99e43c599c1102791f9f4a310e1b49e220b15763
SirPente <t.meacci92@gmail.com>
2023-11-13 07:56:46 +0000

    add procedure_service_request

---
+++ healthcare/healthcare/doctype/procedure_service_request/procedure_service_request.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, earthians Health Informatics Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Procedure Service Request", {
+// 	refresh(frm) {
+
+// 	},
+// });
